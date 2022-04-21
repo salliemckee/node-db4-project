7e1384e04c66c19c70f02f3ad71e370cd4f188ed
Sallie McKee <salliemckee@Morrigan.local>
2022-04-21 14:13:48 -0500

    set up migration

--- knexfile.js
+++ knexfile.js
@@ -1,21 +1,48 @@
-const sharedConfig = {
-  client: "sqlite3",
-  useNullAsDefault: true,
-  migrations: { directory: "./data/migrations" },
-  pool: {
-    afterCreate: (conn, done) => conn.run("PRAGMA foreign_keys = ON", done),
-  },
-};
+// Update with your config settings.
 
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
 module.exports = {
   development: {
-    ...sharedConfig,
-    connection: { filename: "./data/cook_book.db3" },
+    client: "sqlite3",
+    migrations: { directory: "./data/migrations" },
     seeds: { directory: "./data/seeds" },
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => conn.run("PRAGMA foreign_keys = ON", done),
+    },
+    connection: {
+      filename: "./data/cook_book.db3",
+    },
   },
+
   testing: {
-    ...sharedConfig,
-    connection: { filename: "./data/cook_book.db3" },
+    client: "sqlite3",
+    migrations: { directory: "./data/migrations" },
+    seeds: { directory: "./data/seeds" },
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => conn.run("PRAGMA foreign_keys = ON", done),
+    },
+    connection: {
+      filename: "./data/cook_book.db3",
+    },
   },
-  production: {},
+
+  production: {
+    // client: 'postgresql',
+    // connection: {
+    //   database: 'my_db',
+    //   user:     'username',
+    //   password: 'password'
+    // },
+    // pool: {
+    //   min: 2,
+    //   max: 10
+    // },
+    // migrations: {
+    //   tableName: 'knex_migrations'
+    // }
+  },
 };
